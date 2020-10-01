0b4b540ed9f8c34b6ba07a62511559c97a468f61
taxidriver2192 <56368331+taxidriver2192@users.noreply.github.com>
2020-10-01 20:47:08 +0200

    Update Redigeret min .gitignore
    then i can get all the files i need to opdate my github repository.

---
+++ dist/bundle.js
@@ -0,0 +1,176 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/dist/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.htm":
+/*!***********************!*\
+  !*** ./src/index.htm ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "index.htm";
+
+/***/ }),
+
+/***/ "./src/js/index.ts":
+/*!*************************!*\
+  !*** ./src/js/index.ts ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var vm = new Vue({
+    el: "#app",
+    data: {
+        input: "",
+        output: "",
+        str: "",
+        buttons: ["Upper case", "Lower case", "Reverse",],
+        button: "Upper case",
+        totalcharacter: 0,
+        totalword: 0,
+    },
+    methods: {
+        changeSetting: function (index) {
+            this.button = this.buttons[index];
+            if (this.input == 0) {
+                this.output = "Du skal skrive noget tekst i textareaet min lille ven :)";
+            }
+            else if (this.button == "Upper case") {
+                this.output = this.input.toUpperCase();
+            }
+            else if (this.button == "Lower case") {
+                this.output = this.input.toLowerCase();
+            }
+            else if (this.button == "Reverse") {
+                this.output = this.input.split("").reverse().join("");
+            }
+        },
+        charCount: function (input) {
+            this.totalcharacter = this.input.length;
+        },
+        countWords: function () {
+            for (var i = 0; i < this.input.length; i++) {
+                var array = this.input.split(" ");
+                array[i].split("");
+                this.totalword = i;
+            }
+        }
+    }
+});
+
+
+/***/ }),
+
+/***/ "./src/scss/styles.scss":
+/*!******************************!*\
+  !*** ./src/scss/styles.scss ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "bundle.css";
+
+/***/ }),
+
+/***/ 0:
+/*!**********************************************************************!*\
+  !*** multi ./src/index.htm ./src/scss/styles.scss ./src/js/index.ts ***!
+  \**********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! ./src/index.htm */"./src/index.htm");
+__webpack_require__(/*! ./src/scss/styles.scss */"./src/scss/styles.scss");
+module.exports = __webpack_require__(/*! ./src/js/index.ts */"./src/js/index.ts");
+
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=bundle.js.map
